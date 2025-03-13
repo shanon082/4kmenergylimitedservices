da7e508eb1f6abcb05e55db2ad5770d42ea9ba7f
shanon cymon <shanonsimon082@gmail.com>
2025-03-13 12:28:44 +0300

    Construction project @5

--- construction_site/settings.py
+++ construction_site/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-e!t+2qohy(m@z2$x$f+b2wz37q@w^8k40lh(c796c#xrxnl$$2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['fourkmenergylimitedserv.onrender.com']
 
 
 
